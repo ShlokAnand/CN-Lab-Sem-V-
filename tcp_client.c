9a6ec7e975ba1c9b1d67c845ea7b41490cc46939
Shlok Anand <152406547+ShlokAnand@users.noreply.github.com>
2025-11-11 23:21:09 +0530

    tcp_client

--- tcp_client.c
+++ tcp_client.c
@@ -1,28 +1,36 @@
-#include &lt;stdio.h&gt;
-#include &lt;string.h&gt;
-#include &lt;stdlib.h&gt;
-#include &lt;unistd.h&gt;
-#include &lt;netinet/in.h&gt;
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
 #define PORT 10142
 #define BUF_SIZE 1024
+
 int main() {
-int sock;
-struct sockaddr_in server;
-char buffer[BUF_SIZE];
-// Create socket
-sock = socket(AF_INET, SOCK_STREAM, 0);
-server.sin_family = AF_INET;
-server.sin_port = htons(PORT);
-server.sin_addr.s_addr = INADDR_ANY;
-connect(sock, (struct sockaddr *)&amp;server, sizeof(server));
-while (1) {
-printf(&quot;Enter message: &quot;);
-fgets(buffer, BUF_SIZE, stdin);
-send(sock, buffer, strlen(buffer), 0);
-memset(buffer, 0, BUF_SIZE);
-read(sock, buffer, BUF_SIZE);
-printf(&quot;Echo from server: %s&quot;, buffer);
-}
-close(sock);
-return 0;
+    int sock;
+    struct sockaddr_in server;
+    char buffer[BUF_SIZE];
+
+    // Create socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    server.sin_addr.s_addr = INADDR_ANY;
+
+    connect(sock, (struct sockaddr *)&server, sizeof(server));
+
+    while (1) {
+        printf("Enter message: ");
+        fgets(buffer, BUF_SIZE, stdin);
+        send(sock, buffer, strlen(buffer), 0);
+
+        memset(buffer, 0, BUF_SIZE);
+        read(sock, buffer, BUF_SIZE);
+        printf("Echo from server: %s", buffer);
+    }
+
+    close(sock);
+    return 0;
 }
